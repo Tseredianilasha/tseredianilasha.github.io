76cd9b68c89134a5e79d848a9553a0a3bb973962
lasha <tserediani96@gmail.com>
2024-02-07 20:52:39 +0400

    base

---
+++ src/components/intro.tsx
@@ -0,0 +1,7 @@
+export const Intro = () => {
+  return (
+    <div className="w-full h-full items-center justify-center">
+      <span className="text-5xl text-red-500">Welcome 123</span>
+    </div>
+  )
+}
